85f961131663a3c654b30001029984f24e0f8607
Nishon Tandukar <nishon.tan@gmail.com>
2020-07-22 07:48:59 +0545

    Adds landing page

---
+++ js/viz.js
@@ -0,0 +1,41 @@
+
+(function ($) {
+
+  "use strict";
+
+  $(document).ready(function () {
+    
+
+    function scroll_header(){
+      //header-bg change on scroll
+      $(".smooth-scroll").scroll(function(){
+        if($(".smooth-scroll").scrollTop() > 200){
+            $("header").addClass("scroll-header");
+            $("footer").addClass("scroll-footer");
+        }
+        else{
+            $("header").removeClass("scroll-header");
+            $("footer").addClass("scroll-footer");
+        }
+      })
+    }
+    scroll_header();
+
+
+    
+    function header_li(){
+      $('.menu ul li').on('click', 'a', function(){
+        if($(this).closest('a').next().hasClass('active')){
+          $('.menu ul li a').removeClass('active');
+      }
+      else{
+        $('.menu ul li a').removeClass('active');
+      }
+      $(this).closest('a').addClass('active');
+      })
+    }
+    header_li();
+
+  });
+})(window.jQuery);
+
